0b7972429c15df426ed6c9ee1f791c2fa59e6a51
what-the-hub <iamthenight.yeah@gmail.com>
2021-11-24 14:23:58 +0200

    3th-day tasks

---
+++ 3th day tasks/maximumWealth.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+
+const accountsArr = [[1,2,3],[3,2,1],[10,11]]
+const maximumWealth = function(accounts) {
+    let maxSumsArr = []
+
+    const getReduced = (item) => {
+        return item.reduce((total, current) => total + current)
+    }
+
+    accounts.forEach((element) => {
+        maxSumsArr.push(getReduced(element))
+    })
+
+    return Math.max(...maxSumsArr)
+}
+
+console.log(maximumWealth(accountsArr))
