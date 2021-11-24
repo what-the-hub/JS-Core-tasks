0efd5ee7bdcb31feabcc2a96d541cad889875d68
what-the-hub <iamthenight.yeah@gmail.com>
2021-11-24 15:49:47 +0200

    review code

--- 3th day tasks/jewelsAndStones.js
+++ 3th day tasks/jewelsAndStones.js
@@ -5,8 +5,8 @@
  */
 
 const numJewelsInStones = function(jewels, stones) {
-    const spreadStones = [...stones]
-    const result = spreadStones.filter((stone) => jewels.includes(stone))
+    const restStones = [...stones]
+    const result = restStones.filter((stone) => jewels.includes(stone))
     
     return result.length
 };
