24d7e587f19d93d073bbe2736ce10f7f535d85a1
what-the-hub <iamthenight.yeah@gmail.com>
2021-11-28 16:47:18 +0200

    added map to maxWealth function and reviewed code

--- MyScript/index.js
+++ MyScript/index.js
@@ -1,10 +1,11 @@
-const firstWorkDayMs = new Date(2021, 10, 14).getTime()
-const twoDayMs = 86400000 * 2
 //const inputDate = '11/27/2021'
 const inputDate = prompt('Enter the date You are interested in: ', '11/26/2021')
 const inputDateFull = new Date(inputDate)
 
 const checkEnteredDay = (enteredDate) => {
+    const twoDayMs = 86400000 * 2
+    const firstWorkDayMs = new Date(2021, 10, 26).getTime()
+
     const inputDateMs = new Date(enteredDate).getTime()
     const differenceMs = inputDateMs - firstWorkDayMs
     const divisionResult = differenceMs / twoDayMs
@@ -15,51 +16,37 @@ const checkEnteredDay = (enteredDate) => {
     const isSecondFreeDay = !isFirstWorkDay && !checkSecondDay
     const isSecondWorkDay = !isFirstWorkDay && checkSecondDay
 
-
-    /*
-        console.log( divisionResult, 'div result')
-        console.log(divisionResult%2, '__')
-        console.log(reminder, 'reminder')
-    */
-
-
-
     let resultObj = {
         day: '',
         busyness: ''
     }
 
-    if (isSecondWorkDay) {
-        //console.log(reminder)
-
-        resultObj.day = 'second'
-        resultObj.busyness = 'work 2'
-    }
-
-    if (isSecondFreeDay) {
-        //console.log(reminder)
-
-        resultObj.day = 'second'
-        resultObj.busyness = 'free 2'
-    }
-
-    if (isFirstFreeDay) {
-       //console.log(reminder)
-        resultObj.day = 'first'
-        resultObj.busyness = 'FREE'
-    }
     if (isFirstWorkDay) {
-        //console.log(reminder)
-
         resultObj.day = 'first'
         resultObj.busyness = 'WORK'
     }
+    if (isSecondWorkDay) {
+        resultObj.day = 'second'
+        resultObj.busyness = 'WORK'
+    }
+    if (isFirstFreeDay) {
+        resultObj.day = 'first'
+        resultObj.busyness = 'REST'
+    }
+    if (isSecondFreeDay) {
+        resultObj.day = 'second'
+        resultObj.busyness = 'REST'
+    }
+
     return resultObj
 }
+
 const result = checkEnteredDay(inputDate);
+const showLessInfoDay = (fullDate) => fullDate.toString().slice(0, 10)
 
-alert(`This is the ${result.day} day of ${result.busyness}`)
-
+let message = `You entered ${showLessInfoDay(inputDateFull)} \nThis is the ${result.day} day of ${result.busyness}`
+message += '\nCheck console if You want to see current month business'
+alert(message)
 
 const checkEveryDayInMonth = () => {
     const dt = new Date()
@@ -71,10 +58,10 @@ const checkEveryDayInMonth = () => {
     for (let i = 1; i < daysInMonth; i += 1) {
         const day = new Date(year, month, i)
         const result = checkEnteredDay(day)
-        const showLessInfoDay = day.toString().slice(0, 10)
-        const message = `${showLessInfoDay} == ${result.busyness}`
+        const message = `${showLessInfoDay(day)} == ${result.busyness}`
         console.log(message)
     }
 }
+
 checkEveryDayInMonth()
 
